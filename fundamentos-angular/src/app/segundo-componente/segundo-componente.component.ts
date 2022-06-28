b1de3da66c7a430c7fe19d05ef0dd68e10f47fed
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-06-28 15:36:44 -0300

    interpolação e pipe

---
+++ fundamentos-angular/src/app/segundo-componente/segundo-componente.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-segundo-componente',
+  templateUrl: './segundo-componente.component.html',
+  styleUrls: ['./segundo-componente.component.css']
+})
+export class SegundoComponenteComponent  {
+
+  nome: string = "renan";
+  dataNascimento: string = "1990-01-01";
+
+}
