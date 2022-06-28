0cf370d3e9f2433e566aa38d17676f533bc0cf12
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-06-28 16:13:02 -0300

    criação componente personalizado

---
+++ fundamentos-angular/src/app/componente-personalizado/componente-personalizado.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-componente-personalizado',
+  templateUrl: './componente-personalizado.component.html',
+  styleUrls: ['./componente-personalizado.component.css']
+})
+export class ComponentePersonalizadoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
