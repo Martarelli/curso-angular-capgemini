2c6d3c47bda9900fd66be2adc2000f493b4fccae
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-06-28 15:56:58 -0300

    aplicação do two way data binding

--- fundamentos-angular/src/app/two-way-data-binding/two-way-data-binding.component.ts
+++ fundamentos-angular/src/app/two-way-data-binding/two-way-data-binding.component.ts
@@ -5,11 +5,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './two-way-data-binding.component.html',
   styleUrls: ['./two-way-data-binding.component.css']
 })
-export class TwoWayDataBindingComponent implements OnInit {
+export class TwoWayDataBindingComponent  {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  nome: string = "";
 
 }
