4394188b8004d00637b07c6870b52b88c75d9845
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-06-28 15:36:23 -0300

    import pipe

--- fundamentos-angular/src/app/app.module.ts
+++ fundamentos-angular/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrimeiroComponenteComponent } from './primeiro-componente/primeiro.componente.component';
 import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
+import { MultiplicaPorPipe } from './multiplica-por.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrimeiroComponenteComponent,
-    SegundoComponenteComponent
+    SegundoComponenteComponent,
+    MultiplicaPorPipe
   ],
   imports: [
     BrowserModule,
