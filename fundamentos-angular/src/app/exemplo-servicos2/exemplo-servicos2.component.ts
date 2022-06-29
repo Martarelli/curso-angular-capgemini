a1f12e87abda3220682ba4a580a97e4f7e3838f9
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-06-29 17:59:54 -0300

    criado component para exemplo de service

---
+++ fundamentos-angular/src/app/exemplo-servicos2/exemplo-servicos2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exemplo-servicos2',
+  templateUrl: './exemplo-servicos2.component.html',
+  styleUrls: ['./exemplo-servicos2.component.css']
+})
+export class ExemploServicos2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
