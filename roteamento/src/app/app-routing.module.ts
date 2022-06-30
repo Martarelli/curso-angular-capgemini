fa5d0780cb92ffd7f1670879267ee51318f3f5a9
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-06-30 18:33:31 -0300

    adicionada rota para segunda pagina

--- roteamento/src/app/app-routing.module.ts
+++ roteamento/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
+import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
 
 const routes: Routes = [
-  {path: "primeira-pagina", component: PrimeiraPaginaComponent}
+  {path: "primeira-pagina", component: PrimeiraPaginaComponent},
+  {path: "segunda-pagina", component: SegundaPaginaComponent}
 ]
 @NgModule({
   declarations: [],
