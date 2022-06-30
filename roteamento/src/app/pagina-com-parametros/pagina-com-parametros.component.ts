055ce724eb101868e27bb9a8875c6ab34699963f
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-06-30 18:39:47 -0300

    criacao de componente de pagina com parametros

---
+++ roteamento/src/app/pagina-com-parametros/pagina-com-parametros.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina-com-parametros',
+  templateUrl: './pagina-com-parametros.component.html',
+  styleUrls: ['./pagina-com-parametros.component.css']
+})
+export class PaginaComParametrosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
