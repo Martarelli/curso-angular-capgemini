5d131a6e25422025aef17e787a5aa33daa9e2bbf
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-06-30 18:46:23 -0300

    navegando com parametro de rota

--- roteamento/src/app/app-routing.module.ts
+++ roteamento/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
+import { PaginaComParametrosComponent } from './pagina-com-parametros/pagina-com-parametros.component';
 import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
 
 const routes: Routes = [
+  {path: "", redirectTo: "primeira-pagina", pathMatch: "full"},
   {path: "primeira-pagina", component: PrimeiraPaginaComponent},
-  {path: "segunda-pagina", component: SegundaPaginaComponent}
+  {path: "segunda-pagina", component: SegundaPaginaComponent},
+  {path: "pagina-com-parametros/:id", component: PaginaComParametrosComponent}
 ]
 @NgModule({
   declarations: [],
--- roteamento/src/app/pagina-com-parametros/pagina-com-parametros.component.ts
+++ roteamento/src/app/pagina-com-parametros/pagina-com-parametros.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-pagina-com-parametros',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaginaComParametrosComponent implements OnInit {
 
-  constructor() { }
+  id: number | null  = null
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.id = Number(params.get("id"))
+    })
   }
 
 }
