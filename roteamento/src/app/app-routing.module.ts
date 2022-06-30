83d886103416e8d4a625ed485d8aa4c9544c0eed
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-06-30 19:10:31 -0300

    aplicando lazy-loading

--- roteamento/src/app/app-routing.module.ts
+++ roteamento/src/app/app-routing.module.ts
@@ -5,10 +5,11 @@ import { NgModule } from '@angular/core';
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
 
 const routes: Routes = [
-  {path: "", redirectTo: "primeira-pagina", pathMatch: "full"},
-  {path: "primeira-pagina", component: PrimeiraPaginaComponent},
-  {path: "segunda-pagina", component: SegundaPaginaComponent},
-  {path: "pagina-com-parametros/:id", component: PaginaComParametrosComponent}
+  { path: "", redirectTo: "primeira-pagina", pathMatch: "full" },
+  { path: "primeira-pagina", component: PrimeiraPaginaComponent },
+  { path: "segunda-pagina", component: SegundaPaginaComponent },
+  { path: "pagina-com-parametros/:id", component: PaginaComParametrosComponent },
+  { path: 'lazy-loading', loadChildren: () => import('./lazy-loading/lazy-loading.module').then(m => m.LazyLoadingModule) }
 ]
 @NgModule({
   declarations: [],
